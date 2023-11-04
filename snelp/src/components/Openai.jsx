4816d2390b5fb9ce98431dc6abea02f91be2c75a
neilo <neil.carnac@somaiya.edu>
2023-11-04 20:03:52 +0530

    feat: fixed api key

--- snelp/src/components/Openai.jsx
+++ snelp/src/components/Openai.jsx
@@ -2,36 +2,42 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci/completions';
+const OPENAI_API_KEY = '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'; // Replace with your actual API key
 
-function Openai({text}) {
+function Openai({ text }) {
   const [generatedText, setGeneratedText] = useState('');
   const prompt = text;
 
-  async function generateText(prompt) {
-    try {
-      const response = await axios.post(API_ENDPOINT, {
-        prompt,
-        max_tokens: 100,
-        n: 1,
-        stop: '6',
-        temperature: 0.7,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
-        },
-      });
-      console.log(response);
-      setGeneratedText(response.data.choices[0].text.trim());
-    } catch (error) {
-      console.error(error);
-      setGeneratedText("Something went wrong. Please try again later.");
+  useEffect(() => {
+    async function generateText() {
+      try {
+        const response = await axios.post(API_ENDPOINT, {
+          prompt,
+          max_tokens: 100,
+          n: 1,
+          stop: '6',
+          temperature: 0.7,
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+          },
+        });
+        console.log(response);
+        setGeneratedText(response.data.choices[0].text.trim());
+      } catch (error) {
+        console.error(error);
+        setGeneratedText("Something went wrong. Please try again later.");
+      }
     }
-  }
+
+    generateText();
+  }, [prompt]);
+  console.log(prompt)
 
   return (
     <div>
-    <button onClick={() => generateText(prompt)}>Generate Text</button>
+      <button onClick={() => generateText()}>Generate Text</button>
       {generatedText && (
         <p>{generatedText}</p>
       )}
